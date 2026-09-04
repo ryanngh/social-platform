7970338d80fc5a09940f6400bf981f7089ca584d
Nguyen Huu Huynh <sieudoge0@gmail.com>
2026-09-04 21:20:43 +0700

    feat(post): add enums

---
+++ src/main/java/com/ryan/socialplatform/post/enums/MediaType.java
@@ -0,0 +1,7 @@
+package com.ryan.socialplatform.post.enums;
+
+public enum MediaType {
+    IMAGE,
+    VIDEO,
+    GIF
+}
---
+++ src/main/java/com/ryan/socialplatform/post/enums/PostVisibility.java
@@ -0,0 +1,8 @@
+package com.ryan.socialplatform.post.enums;
+
+public enum PostVisibility {
+    PUBLIC,
+    FRIENDS,
+    CLOSE_FRIENDS,
+    PRIVATE
+}
