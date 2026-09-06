2def8981fc4d7861e6cf4edbec1110898002ac9c
Nguyen Huu Huynh <sieudoge0@gmail.com>
2026-09-06 21:06:17 +0700

    fix(post): auto-import wrong  import io.lettuce.core.dynamic.annotation.Param; -> import org.springframework.data.repository.query.Param;

--- src/main/java/com/ryan/socialplatform/post/repository/PostRepository.java
+++ src/main/java/com/ryan/socialplatform/post/repository/PostRepository.java
@@ -1,14 +1,40 @@
 package com.ryan.socialplatform.post.repository;
 
 import com.ryan.socialplatform.post.entity.Post;
-import io.lettuce.core.dynamic.annotation.Param;
+import com.ryan.socialplatform.post.enums.PostVisibility;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.Collection;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface PostRepository extends JpaRepository<Post, UUID> {
 
     Optional<Post> findByIdAndDeletedAtIsNull(UUID id);
+
+    @Query("""
+                SELECT p FROM Post p
+                WHERE p.author.id = :authorId
+                  AND p.deletedAt IS NULL
+                  AND p.visibility IN :visibilities
+            """)
+    Slice<Post> findProfilePosts(
+            @Param("authorId") UUID authorId,
+            @Param("visibilities") Collection<PostVisibility> visibilities,
+            Pageable pageable
+    );
+
+    @Query("""
+                SELECT p FROM Post p
+                WHERE p.author.id = :authorId
+                  AND p.deletedAt IS NULL
+            """)
+    Slice<Post> findOwnProfilePosts(
+            @Param("authorId") UUID authorId,
+            Pageable pageable
+    );
 }
